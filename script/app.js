07118beaaa9d32252917d4aa46954f9fc743ae15
RasFaiO <fylancheros1@misena.edu.co>
2017-09-08 12:46:01 -0500

    actulizacion1

--- script/app.js
+++ script/app.js
@@ -1,8 +1,15 @@
 const objeto = document.getElementById('objeto')
 const btn = document.getElementById('btn')
+const objeto_2 = document.getElementById('objeto_2')
+const boton = document.getElementById('abajo')
 
 btn.addEventListener('click', mover)
 
+boton.addEventListener('click', mover_2)
+
 function mover(){
 	objeto.classList.toggle('esconder_2')
+}
+function mover_2(){
+	objeto_2.classList.toggle('escondido')
 }
---
+++ script/btn.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('#abajo').click(function(){
+		$('body, html').animate({
+			scrollTop: '630px'
+		}, 300);
+	});
+
+});
